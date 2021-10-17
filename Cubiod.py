0613bc04173b28f114de4f49451fee32fea270a7
Brandon White <brandon.white@students.plymouth.ac.uk>
2021-10-17 23:33:34 +0100

    Initial commit

---
+++ Cubiod.py
@@ -0,0 +1,13 @@
+  import StreamReader
+
+class Cubiod:
+    def __init__(self, width, length, height):
+        self.width = width
+        self.length = length
+        self.height = height
+    
+cube = Cubiod(1, 10, 1)
+
+print(cube.width)
+print(cube.length)
+print(cube.height)
