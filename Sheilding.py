f1d36bb5856dba8298fbb042c9513ba742c5c49b
Brandon White <brandon.white@students.plymouth.ac.uk>
2021-10-19 14:00:19 +0100

    Completed Sheilding task.

--- Sheilding.py
+++ Sheilding.py
@@ -6,9 +6,44 @@ h = cube.h
 i = 0
 answer = 0
 
+def funcShortestSide(w, l, h) -> int:
+    temp = 0
+       
+    if(w <= l and w <= h):
+        if(l < h or l == h):
+            temp = w * l
+            return temp
+        else: 
+            temp = w * h
+            return temp
+    
+    elif(l <= w and l <= h):
+        if(w < h or w == h):
+            temp = l * w
+            return temp
+        else: 
+            temp = l * h
+            return temp
+    
+    elif(h <= w and h <= l):
+        if(w < l or w == l):
+            temp = h * w
+            return temp
+        else: 
+            temp = h * l
+            return temp
+
 for x in w:
-    answer = w[i] + l[i] + h[i]
-    print(answer)
+    answer += 2 * (l[i] * w[i]) + 2 * (w[i] * h[i]) + 2 * (l[i] * h[i])
+    answer += funcShortestSide(w[i], l[i], h[i])
+    i = i + 1
+
+print (answer)
+
+
+
+
+
 
 
 
--- StreamReader.py
+++ StreamReader.py
@@ -1,6 +1,6 @@
 from os import close
 
-data = open("example2.txt", "r")
+data = open("example3.txt", "r")
 cuboidArray = []
 width = []
 length = []
@@ -8,9 +8,13 @@ height = []
 i = 0
 
 for x in data:
+     #Append txt file into an array.
      cuboidArray.append(x)
+
+     #Splt the data string into separate nodes at each "x".
      temp = cuboidArray[i].split("x") 
 
+     #Append each node into their respective array's width, length and height. 
      width.append(int(temp[0], base = 10))
      length.append(int(temp[1], base = 10))
      height.append(int(temp[2], base = 10))
