008bfb7c8396472d9e6c42fb5d728d0877d46ca7
Brandon White <brandon.white@students.plymouth.ac.uk>
2021-10-19 15:21:32 +0100

    Changed structure. Added Wiring

---
+++ Main.py
@@ -0,0 +1,25 @@
+from Cubiod import cube
+from Sheilding import funcShortestSide
+from Wiring import func2ShortestSides
+
+w =cube.w 
+l = cube.l
+h = cube.h
+sheilding = 0
+wiring = 0
+i = 0
+y = 0
+
+for x in w:
+    sheilding += 2 * (l[i] * w[i]) + 2 * (w[i] * h[i]) + 2 * (l[i] * h[i])
+    sheilding += funcShortestSide(w[i], l[i], h[i])
+    i = i + 1
+
+print (sheilding)
+
+for x in w:
+    wiring += w[y] * l[y] * h[y]
+    wiring += func2ShortestSides(w[y], l[y], h[y])
+    y = y + 1
+
+print (wiring)
--- Wiring.py
+++ Wiring.py
@@ -1,12 +1,12 @@
 i = 0
 answer = 0
 
-def funcShortestSide(w, l, h) -> int:
+def func2ShortestSides(w, l, h) -> int:
     temp = 0
        
     if(w <= l and w <= h):
         if(l < h or l == h):
-            temp = w * l
+            temp = w + l
             return temp
         else: 
             temp = w * h
@@ -14,7 +14,7 @@ def funcShortestSide(w, l, h) -> int:
     
     elif(l <= w and l <= h):
         if(w < h or w == h):
-            temp = l * w
+            temp = l + w
             return temp
         else: 
             temp = l * h
@@ -22,21 +22,9 @@ def funcShortestSide(w, l, h) -> int:
     
     elif(h <= w and h <= l):
         if(w < l or w == l):
-            temp = h * w
+            temp = h + w
             return temp
         else: 
-            temp = h * l
+            temp = h + l
             return temp
-
-
-
-
-
-
-
-
-
-
-
-
 
